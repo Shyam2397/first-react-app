ddb6e1ee7e261c5a834ca32a52d26d1859b0910c
Shyam2397 <shyamsundarr10@gmail.com>
2023-06-10 16:32:11 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const studentsData = [
+    {name:'sanjay',
+    batch:'b47',
+    edu:'be'
+    },
+    {name:'sanjay',
+    batch:'b47',
+    edu:'be'
+    },
+    {name:'sanjay',
+    batch:'b47',
+    edu:'be'
+    },
+    {name:'sanjay',
+    batch:'b47',
+    edu:'be'
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='card-block'>
+        {studentsData.map((stud,ind)=>(
+          <StudentCard
+          key={ind}
+          name={stud.name}
+          batch={stud.batch}
+          education={stud.edu}/>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function StudentCard(props){
+  return(
+    <div className="student-card">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_J4IeylQzW8W_6kSUF9c7seuRg757hfjyhejz6QFv&s"/>
+      <h3>{props.name}</h3>
+      <p>{props.batch}</p>
+      <p>{props.education}</p>
+    </div>
+  )
+}
